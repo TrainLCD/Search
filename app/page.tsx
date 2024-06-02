7d701c2399fdce146721681bbcabf9c452584fce
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-06-03 05:29:55 +0900

    ハリボテWIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,106 @@
-import Image from "next/image";
+"use client";
+import { ArrowRightIcon } from "@/icons/ArrowRight";
+import { CheckIcon } from "@/icons/Check";
+import { PlusIcon } from "@/icons/Plus";
+import { Input } from "@nextui-org/input";
+import { Listbox, ListboxItem } from "@nextui-org/listbox";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="flex w-screen min-h-screen flex-col items-center justify-center p-8 lg:p-24">
+      <form className="w-full lg:w-1/5">
+        <Input
+          required
+          label="駅名を入力してください"
+          endContent={
+            <div className="h-full flex justify-center items-center">
+              <ArrowRightIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer" />
+            </div>
+          }
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Listbox
+          className="bg-white bg-opacity-80 mt-4 rounded-xl drop-shadow"
+          onAction={(key) => alert(console.debug)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <ListboxItem
+            className="py-4"
+            key="new"
+            endContent={
+              <CheckIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer text-green-500" />
+            }
+          >
+            <p className="font-medium opacity-90">東京駅</p>
+          </ListboxItem>
+        </Listbox>
+        <Input
+          required
+          label="行き先の駅名を入力してください"
+          className="mt-8"
+          endContent={
+            <div className="h-full flex justify-center items-center">
+              <ArrowRightIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer" />
+            </div>
+          }
+        />
+        <Listbox
+          className="bg-white bg-opacity-80 mt-4 rounded-xl drop-shadow"
+          onAction={(key) => alert(console.debug)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <ListboxItem
+            className="py-4"
+            key="new"
+            endContent={
+              <CheckIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer text-green-500" />
+            }
+          >
+            <p className="font-medium opacity-90">半家駅</p>
+          </ListboxItem>
+        </Listbox>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p className="font-medium opacity-90 w-full mt-8">
+          こちらの経路が見つかりました
+        </p>
+        <Listbox
+          className="bg-white bg-opacity-80 mt-4 rounded-xl drop-shadow"
+          onAction={(key) => alert(console.debug)}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <ListboxItem
+            showDivider
+            className="p-4"
+            key="new"
+            endContent={
+              <div className="flex items-center">
+                <PlusIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer" />
+              </div>
+            }
+          >
+            <p className="font-medium opacity-90">東京メトロ東西線 各駅停車</p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <div className="flex items-center mt-1">
+              <p className="text-xs mr-1 opacity-50">中央・総武線 直通</p>
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#009bbf]" />
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#ffd400]" />
+            </div>
+          </ListboxItem>
+          <ListboxItem
+            className="p-4"
+            key="new"
+            endContent={
+              <div className="flex items-center">
+                <PlusIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer" />
+              </div>
+            }
+          >
+            <p className="font-medium opacity-90">東京メトロ副都心線</p>
+            <div className="flex items-center mt-1">
+              <p className="text-xs mr-1 opacity-50">東急線内 急行</p>
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#9c5e31]" />
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#da0442]" />
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#0067c0]" />
+            </div>
+          </ListboxItem>
+        </Listbox>
+      </form>
     </main>
   );
 }
