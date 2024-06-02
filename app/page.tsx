61d8fb6aaaba471657304b16f0f49fa48af6d151
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-06-03 06:49:46 +0900

    駅データを実際にあり得るものに変更

--- app/page.tsx
+++ app/page.tsx
@@ -25,11 +25,21 @@ export default function Home() {
           <ListboxItem
             className="py-4"
             key="new"
+            showDivider
             endContent={
-              <CheckIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer text-green-500" />
+              <CheckIcon className="text-2xl transition-colors flex-shrink-0 cursor-pointer text-green-500" />
             }
           >
-            <p className="font-medium opacity-90">西葛西駅</p>
+            <p className="font-medium opacity-90">渋谷駅</p>
+          </ListboxItem>
+          <ListboxItem
+            className="py-4"
+            key="new"
+            endContent={
+              <CheckIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer" />
+            }
+          >
+            <p className="font-medium opacity-90">高座渋谷駅</p>
           </ListboxItem>
         </Listbox>
         <Input
@@ -49,11 +59,31 @@ export default function Home() {
           <ListboxItem
             className="py-4"
             key="new"
+            showDivider
             endContent={
-              <CheckIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer text-green-500" />
+              <CheckIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer" />
             }
           >
-            <p className="font-medium opacity-90">中野駅</p>
+            <p className="font-medium opacity-90">元町駅</p>
+          </ListboxItem>
+          <ListboxItem
+            className="py-4"
+            key="new"
+            showDivider
+            endContent={
+              <CheckIcon className="text-2xl transition-colors flex-shrink-0 cursor-pointer text-green-500" />
+            }
+          >
+            <p className="font-medium opacity-90">元町・中華街駅</p>
+          </ListboxItem>
+          <ListboxItem
+            className="py-4"
+            key="new"
+            endContent={
+              <CheckIcon className="text-2xl text-default-400 transition-colors flex-shrink-0 cursor-pointer" />
+            }
+          >
+            <p className="font-medium opacity-90">西元町駅</p>
           </ListboxItem>
         </Listbox>
 
@@ -74,12 +104,11 @@ export default function Home() {
               </div>
             }
           >
-            <p className="font-medium opacity-90">東京メトロ東西線 各駅停車</p>
-
-            <div className="flex items-center mt-1">
-              <p className="text-xs mr-1 opacity-50">中央・総武線 直通</p>
-              <div className="w-2 h-2 rounded-full ml-1 bg-[#009bbf]" />
-              <div className="w-2 h-2 rounded-full ml-1 bg-[#ffd400]" />
+            <p className="font-medium opacity-90">東急東横線</p>
+            <div className="flex items-center mt-1 h-4">
+              <p className="text-xs mr-1 opacity-50">みなとみらい線直通</p>
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#da0442]" />
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#0067c0]" />
             </div>
           </ListboxItem>
           <ListboxItem
@@ -91,11 +120,9 @@ export default function Home() {
               </div>
             }
           >
-            <p className="font-medium opacity-90">東京メトロ副都心線 急行</p>
-            <div className="flex items-center mt-1">
-              <p className="text-xs mr-1 opacity-50">東急線内 特急</p>
-              <div className="w-2 h-2 rounded-full ml-1 bg-[#9c5e31]" />
-              <div className="w-2 h-2 rounded-full ml-1 bg-[#da0442]" />
+            <p className="font-medium opacity-90">湘南新宿ライン</p>
+            <div className="flex items-center mt-1 h-4">
+              <div className="w-2 h-2 rounded-full ml-1 bg-[#e31f26]" />
               <div className="w-2 h-2 rounded-full ml-1 bg-[#0067c0]" />
             </div>
           </ListboxItem>
