f5caccbc7bea36de639ebf4f765c751633dd9f9a
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-09-07 17:44:23 +0900

    凡例に停車追加

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,7 @@ type Inputs = {
 };
 
 const STOP_CONDITION_MAP = {
+  0: { text: "停車", color: "black" },
   1: { text: "通過", color: "gray-400" },
   2: { text: "一部通過", color: "yellow-400" },
   3: { text: "平日通過", color: "blue-400" },
